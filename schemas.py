fdc5e03619e9673c1b3c24ab87f69a820ae16ede
Bugrahan Donmez <donmezbugrahan@gmail.com>
2024-04-02 14:37:45 +0300

    Update input type

--- schemas.py
+++ schemas.py
@@ -1,8 +1,7 @@
 import pydantic as _pydantic
-from typing import Optional
-from fastapi import UploadFile
+from typing import List
 
 
 class _QueryBase(_pydantic.BaseModel):
-    encoded_base_img: UploadFile
+    encoded_base_img: List[str]
     gender: str
--- services.py
+++ services.py
@@ -1,8 +1,7 @@
 import schemas as _schemas
 from PIL import Image
 from io import BytesIO
-import numpy as np
-from pkg_resources import parse_version
+import base64
 import utils
 import uuid
 import cv2
@@ -19,8 +18,7 @@ async def find_look_alike(query: _schemas._QueryBase) -> Image:
     temp_id = str(uuid.uuid4())
     utils.create_temp()
     
-    request_object_content = await query.encoded_base_img.read()
-    init_image = Image.open(BytesIO(request_object_content))
+    init_image = Image.open(BytesIO(base64.b64decode(query.encoded_base_img[0])))
     init_image = init_image.save(utils.TEMP_PATH + '/' + temp_id + '.jpg')
     
     results = utils.find_similar_face(utils.TEMP_PATH + '/' + temp_id + '.jpg', df, query.gender)
